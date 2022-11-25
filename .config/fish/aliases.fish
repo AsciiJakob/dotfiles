c36a87190f898cc4830b0db4068438f2a9cff715
Jakob <jakobjakobjakob2@gmail.com>
2022-11-25 15:09:33 +0100

    add ass function

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,3 +1,9 @@
 alias g git
 alias ru 'xbps-install -S'
 alias upd 'sudo xbps-install -Su'
+function ass
+	set file (string split . $argv)
+	set file $file[1]
+	as $argv -o $file.o
+	ld $file.o -o $file
+ end
