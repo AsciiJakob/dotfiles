f372210c45fedb0a88a580f95e34ff13aaa3aec7
Jakob <jakobjakobjakob2@gmail.com>
2023-07-22 23:32:48 +0200

    modified:   .config/fish/aliases.fish

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -2,7 +2,9 @@ alias g git
 alias ru 'xbps-install -S'
 alias upd 'sudo xbps-install -Su'
 alias bai 'sudo shutdown -h now'
+alias untar 'tar -xvzf'
 alias findpid 'ps aux | grep '
+alias s 'kitty +kitten ssh'
 function ass
 	set file (string split . $argv)
 	set file $file[1]
